7035c2babaee8489c451a42a994912de680a9298
eadesany <elijah.adesanya@oulu.fi>
2023-01-23 19:09:13 +0000

    terrafoam commit

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -3,3 +3,7 @@ resource "aws_instance" "test" {
   ami               = "ami-0a017d8ceb274537d"
   instance_type     = "t3.micro"
 }
+
+output "public_ip" {
+  value = aws_instance.test[0].public_ip
+}
